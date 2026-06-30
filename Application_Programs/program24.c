4bfe4a6a5e4ec642a4b1767233f5c298384fb662
shraddhhhaaa <shraddhamune8@gmail.com>
2026-06-30 22:12:13 +0530

    Initial upload

---
+++ Application_Programs/program24.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    //sequence
+    
+    printf("JAY GANESH...\n");
+    printf("JAY GANESH...\n");
+    printf("JAY GANESH...\n");
+    printf("JAY GANESH...\n");
+    
+    return 0;
+}
---
+++ Application_Programs/program25.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    //iteration
+
+    for(iCnt=1; iCnt<=4; iCnt++)
+    {
+         printf("JAY GANESH...\n");
+    }
+    return 0;
+}
