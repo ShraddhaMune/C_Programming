170978cc2b550e2fcb2ffd2db66511e1ee50a418
shraddhhhaaa <shraddhamune8@gmail.com>
2026-07-05 23:27:58 +0530

    initial upload

---
+++ Application_Programs/program27.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+    int iValue = 0;
+
+    printf("Enter the Frequency : ");
+    scanf("%d",&iValue);
+
+    for(iCnt=1; iCnt<=iValue; iCnt++)
+    {
+         printf("JAY GANESH...\n");
+    }
+    return 0;
+}
